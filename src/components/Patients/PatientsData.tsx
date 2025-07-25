b4c2df8eb2281c856c20a1725c92e1f924698f3f
bocuyacuy44 <yusupmuhamad9945@gmail.com>
2025-07-25 09:15:20 +0700

    Updated PatientsData.tsx

--- src/components/Patients/PatientsData.tsx
+++ src/components/Patients/PatientsData.tsx
@@ -95,7 +95,7 @@ const PatientsData: React.FC<PatientsDataProps> = ({
           className="flex items-center space-x-2 px-4 py-2 bg-blue-600 text-white rounded-lg font-medium hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
         >
           <Plus className="w-5 h-5" />
-          <span>Tambah Data Pasien</span>
+          <span>Tambah Pasien Baru</span>
         </button>
       </div>
 
